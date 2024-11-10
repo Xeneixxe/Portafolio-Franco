5ffe09c59676520a3472597c7fcc65028a5ba876
Franco Morales <francomorale36@gmail.com>
2024-11-09 21:33:36 -0300

    Portfolio Personal

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import DataComponente from './components/DataComponente.vue'
+import NavComponent from './components/NavComponent.vue'
+import DatosPersonales from './components/DatosPersonales.vue'
+import EducacionComponent from './components/EducacionComponent.vue'
+import ProyectosComponente from './components/ProyectosComponente.vue'
+import FooterComponente from './components/FooterComponente.vue'
+import SectionTitle from './components/SectionTitle.vue'
+import HabilidadesComponente from './components/HabilidadesComponente.vue'
+</script>
+
+<template>
+  <div class="bg-effect-0"></div>
+  <div class="bg-effect-1"></div>
+  <div class="bg-effect-2"></div>
+  <NavComponent />
+  <span id="about"></span>
+  <SectionTitle children="Acerca" />
+  <DatosPersonales />
+  <span id="ip"></span>
+  <SectionTitle children="Welcome" />
+  <div class="wrap">
+    <h1>Bienvenido desde<DataComponente /></h1>
+  </div>
+  <span id="Education"></span>
+  <SectionTitle children="Educación" />
+  <EducacionComponent />
+  <span id="proyects"></span>
+  <SectionTitle children="Proyectos" />
+
+  <ProyectosComponente />
+
+  <span id="hablidades"></span>
+  <SectionTitle children="Habilidades" />
+  <HabilidadesComponente />
+  <section id="intereses"><h2>Intereses</h2></section>
+  <FooterComponente />
+</template>
+
+<style scoped>
+.wrap {
+  text-align: center;
+}
+</style>
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,55 @@
+<script lang="js" setup></script>
+
+<template>
+  <nav class="navbar">
+    <div class="navbar-menu">
+      <ul>
+        <a
+          v-for="nav in navegacion"
+          :key="nav.nombre"
+          href="nav.enlace"
+          class="nav.item"
+          >{{ nav.nombre }}</a
+        >
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const navegacion = ref([
+  { id: 1, nombre: 'Educacion', enlace: '#educacion' },
+  { id: 2, nombre: 'Experiencia', enlace: '#experiencia' },
+  { id: 3, nombre: 'Proyectos', enlace: '#proyectos' },
+  { id: 4, nombre: 'Habilidades', enlace: '#habilidades' },
+])
+</script>
+
+<style scoped>
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  background: linear-gradient(to top, #22222275, #050505d2);
+  padding: 1rem;
+  border-bottom: 1px solid #333;
+  z-index: 99;
+}
+ul {
+  display: flex;
+  justify-content: space-around;
+  list-style-type: none;
+}
+li {
+  color: #ff8500; /* Color naranja estilo Bleach */
+}
+a {
+  color: inherit;
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+</style>
